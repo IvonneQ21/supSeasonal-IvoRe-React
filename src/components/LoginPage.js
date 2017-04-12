8df7a8531493e47bdcba00f4d54b3e2f78a58aab
Ivonne Terrero <iqcodes21@gmail.com>
2017-04-12 01:32:05 -0700

    login working

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,6 +3,10 @@ import { Field, reduxForm } from 'redux-form';
 import {bindActionCreators} from 'redux';
 import {findUser} from '../actions/index';
 import {connect} from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
+// import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 
 
@@ -10,21 +14,28 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return bindActionCreators({findUser}, dispatch);
 }
 
-const mapStateToProps = (state) =>{
-  return {
-    loggedIn: false
-  }
-}
+
+// const mapStateToProps = (state) =>{
+//   return {
+//     loggedIn: false
+//   }
+// }
 
 const style = {
-  color: 'blue'
+  color: 'blue',
+  backgroundColor: 'grey',
+  labelColor: "blue"
+
 }
+
+/*
 class UserLogIn extends Component {
 
   render() {
     const { handleSubmit } = this.props.findUser;
     return (
       <div style={style} className="loginForm">
+
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email</label>
@@ -34,13 +45,35 @@ class UserLogIn extends Component {
           <label htmlFor="password">Password</label>
           <Field name="password" component="input" type="password" label="input"/>
         </div>
-        <button type="submit">Submit</button>
+        <a href="/home"><button type="submit">Submit</button></a>
       </form>
       </div>
     );
   }
 }
 
+*/
+
+class UserLogIn extends Component {
+
+  render() {
+    const { handleSubmit } = this.props.findUser;
+    return (
+      <div style={style} className="loginForm">
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" label="input"/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field name="password" component="input" type="password" label="input"/>
+        </div>
+        <RaisedButton a href="/home" label="submit"/>
+      </div>
+    );
+  }
+}
 
 export default connect(null, mapDispatchToProps)(reduxForm({ form:'LoginForm'})(UserLogIn));
 // export default UserLogIn;
